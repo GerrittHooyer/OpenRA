29a048280140e2e52484b4e2505ba391c2174d59
GerrittHooyer <gerritt.hooyer@gmail.com>
2026-04-22 23:37:09 -0400

    Displaying selected units

--- OpenRA.Mods.Common/Widgets/Logic/StatusBarLabelLogic.cs
+++ OpenRA.Mods.Common/Widgets/Logic/StatusBarLabelLogic.cs
@@ -1,21 +1,44 @@
-﻿using OpenRA.Widgets;
+﻿using System.Collections.Generic;
+using System.Linq;
+using OpenRA.Support;
+using OpenRA.Widgets;
 
 namespace OpenRA.Mods.Common.Widgets.Logic
 {
 	class StatusBarLabelLogic : ChromeLogic
 	{
+
+		readonly LabelWidget widget;
+		readonly World world;
+		Actor[] actors;
+
 		[ObjectCreator.UseCtor]
 		public StatusBarLabelLogic(LabelWidget widget, World world)
 		{
-			var players = "";
-			var i = 1;
+			this.widget = widget;
+			this.world = world;
+		}
 
-			foreach (var player in world.Players)
+		public override void Tick()
+		{
+			var actors = world.Selection.Actors.Where(a => a.Owner == world.LocalPlayer && a.IsInWorld && !a.IsDead).ToArray();
+			var map = new Dictionary<string, int>();
+			var text = "";
+
+			foreach (var a in actors)
 			{
-				players += $"{i++}. {player.PlayerName} \n";
+				var name = a.Info.Name;
+				map.TryAdd(name, 0);
+				map[name]++;
 			}
 
-			widget.GetText = () => players;
+			foreach (var key in map.Keys)
+			{
+				text += $"{key} : {map[key]}\n";
+			}
+
+			widget.GetText = () => text;
+			base.Tick();
 		}
 	}
 }
