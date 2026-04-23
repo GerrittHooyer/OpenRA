e07860ac67aad2aef8bf58ffe6ecc893b21c4af7
GerrittHooyer <gerritt.hooyer@gmail.com>
2026-04-22 23:14:03 -0400

    Drew a box in the bottom left
    It writes out all the active Players in the World object  :)

---
+++ OpenRA.Mods.Common/Widgets/Logic/Ingame/StatusBarLogic.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections.Generic;
+using OpenRA.Widgets;
+
+namespace OpenRA.Mods.Common.Widgets.Logic.Ingame
+{
+	class StatusBarLogic : ChromeLogic
+	{
+		[ObjectCreator.UseCtor]
+		public StatusBarLogic(Widget widget, World world, Dictionary<string, MiniYaml> logicArgs)
+		{
+		}
+	}
+}
---
+++ OpenRA.Mods.Common/Widgets/Logic/StatusBarLabelLogic.cs
@@ -0,0 +1,21 @@
+﻿using OpenRA.Widgets;
+
+namespace OpenRA.Mods.Common.Widgets.Logic
+{
+	class StatusBarLabelLogic : ChromeLogic
+	{
+		[ObjectCreator.UseCtor]
+		public StatusBarLabelLogic(LabelWidget widget, World world)
+		{
+			var players = "";
+			var i = 1;
+
+			foreach (var player in world.Players)
+			{
+				players += $"{i++}. {player.PlayerName} \n";
+			}
+
+			widget.GetText = () => players;
+		}
+	}
+}
